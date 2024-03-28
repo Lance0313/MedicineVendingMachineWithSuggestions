49a4012fce4f967f9977bc7dbd674541d9f320c3
nickamaes <138411867+nickamaes@users.noreply.github.com>
2024-03-29 00:02:10 +0800

    Revised start screen

--- StartScreen.Designer.cs
+++ StartScreen.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(StartScreen));
             this.StartScreenPurchase = new ePOSOne.btnProduct.RoundButton();
             this.StartScreenLabel = new System.Windows.Forms.Label();
             this.StartScreenSuggestion = new ePOSOne.btnProduct.RoundButton();
@@ -41,11 +42,11 @@
             this.StartScreenPurchase.ButtonColor = System.Drawing.Color.FromArgb(((int)(((byte)(254)))), ((int)(((byte)(222)))), ((int)(((byte)(88)))));
             this.StartScreenPurchase.FlatAppearance.BorderSize = 0;
             this.StartScreenPurchase.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.StartScreenPurchase.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.StartScreenPurchase.Location = new System.Drawing.Point(142, 279);
+            this.StartScreenPurchase.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.StartScreenPurchase.Location = new System.Drawing.Point(138, 297);
             this.StartScreenPurchase.Name = "StartScreenPurchase";
-            this.StartScreenPurchase.OnHoverBorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(254)))), ((int)(((byte)(222)))), ((int)(((byte)(88)))));
-            this.StartScreenPurchase.OnHoverButtonColor = System.Drawing.Color.FromArgb(((int)(((byte)(254)))), ((int)(((byte)(222)))), ((int)(((byte)(88)))));
+            this.StartScreenPurchase.OnHoverBorderColor = System.Drawing.Color.Gold;
+            this.StartScreenPurchase.OnHoverButtonColor = System.Drawing.Color.Gold;
             this.StartScreenPurchase.OnHoverTextColor = System.Drawing.Color.Black;
             this.StartScreenPurchase.Size = new System.Drawing.Size(208, 46);
             this.StartScreenPurchase.TabIndex = 0;
@@ -58,13 +59,14 @@
             // 
             this.StartScreenLabel.AutoSize = true;
             this.StartScreenLabel.BackColor = System.Drawing.Color.Transparent;
-            this.StartScreenLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.StartScreenLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 50F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.StartScreenLabel.ForeColor = System.Drawing.SystemColors.ButtonFace;
-            this.StartScreenLabel.Location = new System.Drawing.Point(71, 61);
+            this.StartScreenLabel.Location = new System.Drawing.Point(49, 46);
             this.StartScreenLabel.Name = "StartScreenLabel";
-            this.StartScreenLabel.Size = new System.Drawing.Size(679, 55);
+            this.StartScreenLabel.Size = new System.Drawing.Size(713, 152);
             this.StartScreenLabel.TabIndex = 1;
-            this.StartScreenLabel.Text = "SMART Pharmaceutical Kiosk ";
+            this.StartScreenLabel.Text = "SMART \r\nPharmaceutical Kiosk ";
+            this.StartScreenLabel.TextAlign = System.Drawing.ContentAlignment.TopCenter;
             this.StartScreenLabel.Click += new System.EventHandler(this.StartScreenLabel_Click);
             // 
             // StartScreenSuggestion
@@ -73,11 +75,11 @@
             this.StartScreenSuggestion.ButtonColor = System.Drawing.Color.FromArgb(((int)(((byte)(254)))), ((int)(((byte)(222)))), ((int)(((byte)(88)))));
             this.StartScreenSuggestion.FlatAppearance.BorderSize = 0;
             this.StartScreenSuggestion.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.StartScreenSuggestion.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.StartScreenSuggestion.Location = new System.Drawing.Point(452, 279);
+            this.StartScreenSuggestion.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.StartScreenSuggestion.Location = new System.Drawing.Point(457, 297);
             this.StartScreenSuggestion.Name = "StartScreenSuggestion";
-            this.StartScreenSuggestion.OnHoverBorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(254)))), ((int)(((byte)(222)))), ((int)(((byte)(88)))));
-            this.StartScreenSuggestion.OnHoverButtonColor = System.Drawing.Color.FromArgb(((int)(((byte)(254)))), ((int)(((byte)(222)))), ((int)(((byte)(88)))));
+            this.StartScreenSuggestion.OnHoverBorderColor = System.Drawing.Color.Gold;
+            this.StartScreenSuggestion.OnHoverButtonColor = System.Drawing.Color.Gold;
             this.StartScreenSuggestion.OnHoverTextColor = System.Drawing.Color.Black;
             this.StartScreenSuggestion.Size = new System.Drawing.Size(208, 46);
             this.StartScreenSuggestion.TabIndex = 2;
@@ -89,11 +91,11 @@
             // 
             this.label1.AutoSize = true;
             this.label1.BackColor = System.Drawing.Color.Transparent;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 17F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.SystemColors.ButtonFace;
-            this.label1.Location = new System.Drawing.Point(71, 144);
+            this.label1.Location = new System.Drawing.Point(29, 198);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(682, 29);
+            this.label1.Size = new System.Drawing.Size(741, 29);
             this.label1.TabIndex = 3;
             this.label1.Text = "Streamlined Over-the-Counter Medicine Dispensing Innovation\r\n";
             // 
@@ -103,7 +105,7 @@
             this.StartScreenFooter.BackColor = System.Drawing.Color.Transparent;
             this.StartScreenFooter.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.StartScreenFooter.ForeColor = System.Drawing.SystemColors.AppWorkspace;
-            this.StartScreenFooter.Location = new System.Drawing.Point(253, 421);
+            this.StartScreenFooter.Location = new System.Drawing.Point(261, 428);
             this.StartScreenFooter.Name = "StartScreenFooter";
             this.StartScreenFooter.Size = new System.Drawing.Size(270, 20);
             this.StartScreenFooter.TabIndex = 4;
@@ -113,7 +115,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackgroundImage = global::ThesisBeta.Properties.Resources.Home;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.ClientSize = new System.Drawing.Size(800, 480);
             this.Controls.Add(this.StartScreenFooter);
