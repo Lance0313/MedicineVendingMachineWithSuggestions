b34efc5c823a5fea98339a2672e54a2be541306e
Lance0313 <83895669+Lance0313@users.noreply.github.com>
2024-04-12 21:59:48 +0800

    Update CashPayment.cs

--- CashPayment.cs
+++ CashPayment.cs
@@ -1,8 +1,9 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO.Ports;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,11 +11,64 @@ using System.Windows.Forms;
 
 namespace ThesisBeta
 {
-    public partial class CashPayment : UserControl
+    public partial class CashPayment : Form
     {
+        private SerialPort serialPort;
         public CashPayment()
         {
             InitializeComponent();
+            InitializeSerialPort();
+            try
+            {
+                serialPort.Open();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error opening serial port: " + ex.Message);
+            }
+            TotalAmountValue.Text = "Php " +Purchase.totalPrice.ToString();
+            CashPaymentProceed.Visible = false;
+        }
+        private void InitializeSerialPort()
+
+        {
+            serialPort = new SerialPort("COM4", 9600);
+            serialPort.DataReceived += SerialPort_DataReceived; 
+        }
+        private void SerialPort_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            string data = serialPort.ReadLine();
+
+            TotalInsertedAmount.Invoke((MethodInvoker)(() => TotalInsertedAmount.Text = data));
+        }
+        private void BackButton_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void TotalInsertedAmount_TextChanged(object sender, EventArgs e)
+        {
+            if (int.TryParse(TotalInsertedAmount.Text.Trim(), out int amountInserted))
+            {
+                if (Purchase.totalPrice <= amountInserted)
+                {
+                    CashPaymentProceed.Visible = true;
+                }
+                else
+                {
+                    CashPaymentProceed.Visible = false;
+                }
+            }
+            else
+            {
+                CashPaymentProceed.Visible = false;
+                Console.WriteLine("MAli");
+            }
+        }
+
+        private void InitializeEventListeners(object sender, EventArgs e)
+        {
+            TotalInsertedAmount.TextChanged += TotalInsertedAmount_TextChanged;
         }
     }
 }
