2772284dfb0d0de7890a056c81eb6f75840d8a0f
Lance0313 <83895669+Lance0313@users.noreply.github.com>
2024-04-12 22:06:02 +0800

    Update PaymentOption.Designer.cs

--- PaymentOption.Designer.cs
+++ PaymentOption.Designer.cs
@@ -1,4 +1,4 @@
-﻿namespace ThesisBeta
+namespace ThesisBeta
 {
     partial class PaymentOption
     {
@@ -47,45 +47,51 @@
             this.PaymentOptionPanel.Controls.Add(this.label1);
             this.PaymentOptionPanel.Controls.Add(this.pictureBox1);
             this.PaymentOptionPanel.Controls.Add(this.PaymentOptionGcash);
-            this.PaymentOptionPanel.Location = new System.Drawing.Point(43, 47);
+            this.PaymentOptionPanel.Location = new System.Drawing.Point(57, 58);
+            this.PaymentOptionPanel.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.PaymentOptionPanel.Name = "PaymentOptionPanel";
-            this.PaymentOptionPanel.Size = new System.Drawing.Size(412, 224);
+            this.PaymentOptionPanel.Size = new System.Drawing.Size(549, 276);
             this.PaymentOptionPanel.TabIndex = 1;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(277, 191);
+            this.label2.Location = new System.Drawing.Point(369, 235);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(31, 13);
+            this.label2.Size = new System.Drawing.Size(38, 16);
             this.label2.TabIndex = 3;
             this.label2.Text = "Cash";
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(88, 191);
+            this.label1.Location = new System.Drawing.Point(117, 235);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(38, 13);
+            this.label1.Size = new System.Drawing.Size(46, 16);
             this.label1.TabIndex = 2;
             this.label1.Text = "Gcash";
             // 
             // pictureBox1
             // 
             this.pictureBox1.Image = global::ThesisBeta.Properties.Resources.Peso;
-            this.pictureBox1.Location = new System.Drawing.Point(212, 15);
+            this.pictureBox1.Location = new System.Drawing.Point(283, 18);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(188, 162);
+            this.pictureBox1.Size = new System.Drawing.Size(251, 199);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox1.TabIndex = 1;
             this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
             // 
             // PaymentOptionGcash
             // 
             this.PaymentOptionGcash.Image = global::ThesisBeta.Properties.Resources.Gcash;
-            this.PaymentOptionGcash.Location = new System.Drawing.Point(12, 15);
+            this.PaymentOptionGcash.Location = new System.Drawing.Point(16, 18);
+            this.PaymentOptionGcash.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.PaymentOptionGcash.Name = "PaymentOptionGcash";
-            this.PaymentOptionGcash.Size = new System.Drawing.Size(188, 162);
+            this.PaymentOptionGcash.Size = new System.Drawing.Size(251, 199);
             this.PaymentOptionGcash.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.PaymentOptionGcash.TabIndex = 0;
             this.PaymentOptionGcash.TabStop = false;
@@ -94,9 +100,10 @@
             // PaymentOptionClose
             // 
             this.PaymentOptionClose.Image = global::ThesisBeta.Properties.Resources.Yellow_X;
-            this.PaymentOptionClose.Location = new System.Drawing.Point(456, 3);
+            this.PaymentOptionClose.Location = new System.Drawing.Point(608, 4);
+            this.PaymentOptionClose.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.PaymentOptionClose.Name = "PaymentOptionClose";
-            this.PaymentOptionClose.Size = new System.Drawing.Size(41, 35);
+            this.PaymentOptionClose.Size = new System.Drawing.Size(55, 43);
             this.PaymentOptionClose.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.PaymentOptionClose.TabIndex = 2;
             this.PaymentOptionClose.TabStop = false;
@@ -104,13 +111,14 @@
             // 
             // PaymentOption
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(252)))), ((int)(((byte)(84)))), ((int)(((byte)(134)))));
-            this.ClientSize = new System.Drawing.Size(500, 300);
+            this.ClientSize = new System.Drawing.Size(667, 369);
             this.Controls.Add(this.PaymentOptionClose);
             this.Controls.Add(this.PaymentOptionPanel);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "PaymentOption";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "PaymentOption";
