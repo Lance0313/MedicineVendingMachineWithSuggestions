e7593571b67511201ebff3919c38e1d070fcf485
Lance0313 <83895669+Lance0313@users.noreply.github.com>
2024-04-12 22:06:32 +0800

    Update Thankyou.cs

--- Thankyou.cs
+++ Thankyou.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO.Ports;
 
 namespace ThesisBeta
 {
@@ -16,8 +17,43 @@ namespace ThesisBeta
         public Thankyou()
         {
             InitializeComponent();
+
+            
+            for (int i = 0; i < Purchase.itemNames.Count; i++)
+            {
+                string itemName = Purchase.itemNames[i];
+                int quantity = Purchase.quantities[i];
+
+                switch (itemName)
+                {
+                    case "Advil":
+                        ActivateMotorBasedOnQuantity(motorControl, quantity, motorControl.ActivateMotor1);
+                        break;
+                    case "Neozep":
+                        ActivateMotorBasedOnQuantity(motorControl, quantity, motorControl.ActivateMotor2);
+                        break;
+                    case "Biogesic":
+                        ActivateMotorBasedOnQuantity(motorControl, quantity, motorControl.ActivateMotor3);
+                        break;
+                    case "Diatabs":
+                        ActivateMotorBasedOnQuantity(motorControl, quantity, motorControl.ActivateMotor4);
+                        break;
+                    case "Ascorbic":
+                        ActivateMotorBasedOnQuantity(motorControl, quantity, motorControl.ActivateMotor5);
+                        break;
+                    // Add cases for other items
+                    default:
+                        Console.WriteLine($"No motor activation found for item: {itemName}");
+                        break;
+                }
+            }
+        }
+
+        private void BacktoStartButton_Click(object sender, EventArgs e)
+        {
             
         }
+
         private void ActivateMotorBasedOnQuantity(MotorControl motorControl, int quantity, Action activateMotor)
         {
             // Activate the motor based on the quantity
@@ -26,7 +62,6 @@ namespace ThesisBeta
                 activateMotor();
             }
         }
-
-
     }
 }
+
